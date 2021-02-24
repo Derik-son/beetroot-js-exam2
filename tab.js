cf5786e95260d253cd6fba2833dd64a9a8367c90
Derik-son <taras.timoshuk93@gmail.com>
2021-02-24 20:23:25 +0200

    third-section

---
+++ tab.js
@@ -0,0 +1,23 @@
+function openTab(evt, name) {
+   var x = window.matchMedia("(max-width: 1200px)")
+   let i, tabcontent, tablinks;
+   tabcontent = document.getElementsByClassName("tabcontent");
+   
+   for (i = 0; i < tabcontent.length; i++) {
+       tabcontent[i].style.display = "none";
+   }
+   tablinks = document.getElementsByClassName("tablinks");
+   
+   for (i = 0; i < tablinks.length; i++) {
+       tablinks[i].className = tablinks[i].className.replace(" active", "");
+   }
+
+   document.getElementById(name).style.display = "block";
+   evt.currentTarget.className += " active";
+   
+
+
+   
+}
+
+document.getElementById("defaultOpen").click();
